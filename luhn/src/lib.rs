880269f1df010e544c2532b5991f3475f3fdb955
noah-clements <noahclements@gmail.com>
2023-03-31 02:01:05 -0400

    incorporated try_fold and maps from JaneL's mentored solution

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,28 +1,11 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let mut digits: Vec<u32> = Vec::new();
-    for (i, c) in code.chars().rev()
+    code.chars().rev()
     .filter(|c| !c.is_whitespace())
-    .enumerate() {
-        if c.is_digit(10) {
-            if i % 2 == 1 {
-                let d = c.to_digit(10).unwrap() * 2;
-                if d > 9 {
-                    digits.push(d - 9);
-                } else {
-                    digits.push(d);
-                }
-            } else {
-                digits.push(c.to_digit(10).unwrap());
-            }
-        } else if c != ' ' {
-            return false;
-        }
-    }
-    let sum: u32 = digits.iter().sum();
-    if digits.len() > 1 && sum % 10 == 0 {
-        true
-    } else {
-        false
-    }
+    .try_fold((0, 0), |(sum, count), val| {
+        val.to_digit(10)
+            .map(|num| if count % 2 == 1 { num * 2 } else { num })
+            .map(|num| if num > 9 { num - 9 } else { num })
+            .map(|num| (num + sum, count + 1))
+    }).map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
 }
