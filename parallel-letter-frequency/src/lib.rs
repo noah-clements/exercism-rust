1199bc28ef1aca503f5e472327bc460e9be727d1
noah-clements <noahclements@gmail.com>
2023-04-01 04:10:42 -0400

    now beats the sequential benchmark significantly **for large parallel** small input is better with sequential

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,36 +1,46 @@
 use std::collections::HashMap;
 use std::sync::mpsc;
 use std::thread;
-use std::cmp::min;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    // let slice_size = input.len() / worker_count;
-    // let remainder = input.len() % worker_count;
+    let slice_size = input.len() / worker_count;
+    let remainder = input.len() % worker_count;
     let mut all_results: HashMap<char, usize> = HashMap::new();
     let (tx,rx) = mpsc::channel();
-    let mut phrases = input.iter();
-    while phrases.len() > 0 {
-        for id in 0..min(worker_count, phrases.len()) {
-            if let Some(phrase) = phrases.next() {
-                let thread_tx = tx.clone();
-                let phrase = phrase.to_lowercase().to_string();
-                thread::spawn(move || {
-                    let mut result = HashMap::new();
-                    for c in phrase.chars() {
-                        if c.is_alphabetic() {
-                            let count = result.entry(c).or_insert(0);
-                            *count += 1;
-                        }
-                    }
-                    thread_tx.send(result).unwrap();
-                    println!("Thread {} finished", id);
-                });
+
+    for id in 0..worker_count {
+        let phrase = input[id*slice_size..(id+1)*slice_size].iter().fold(String::new(), |mut acc, &x| {
+            acc.push_str(x);
+            acc
+        });
+        let thread_tx = tx.clone();
+        thread::spawn(move || {
+            let mut result = HashMap::new();
+            for c in phrase.to_lowercase().to_string().chars() {
+                if c.is_alphabetic() {
+                    let count = result.entry(c).or_insert(0);
+                    *count += 1;
+                }
+            }
+            thread_tx.send(result).unwrap();
+        });
+    }
+    drop(tx);
+
+    // handle remainder
+    if remainder > 0 {
+        let phrase = input[worker_count*slice_size..].iter().fold(String::new(), |mut acc, &x| {
+            acc.push_str(x);
+            acc
+        });
+        for c in phrase.to_lowercase().to_string().chars() {
+            if c.is_alphabetic() {
+                let count = all_results.entry(c).or_insert(0);
+                *count += 1;
             }
         }
     }
-    drop(tx);
     for received in rx {
-        println!("{:?}", received);
         for (key, value) in received {
             let count = all_results.entry(key).or_insert(0);
             *count += value;
