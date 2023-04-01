9b379b723324e9d9c596de63017b49c95455c3c7
noah-clements <noahclements@gmail.com>
2023-04-01 04:51:30 -0400

    cleanup and added times

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -2,6 +2,15 @@ use std::collections::HashMap;
 use std::sync::mpsc;
 use std::thread;
 
+// my times - tried some community solutions (only safe), 
+// but if I want to use channels, this is the time I'm going to get
+// test bench_large_parallel   ... bench:     165,093 ns/iter (+/- 25,879)
+// test bench_large_sequential ... bench:     316,714 ns/iter (+/- 19,144)
+// test bench_small_parallel   ... bench:      46,569 ns/iter (+/- 18,586)
+// test bench_small_sequential ... bench:      11,322 ns/iter (+/- 968)
+// test bench_tiny_parallel    ... bench:      36,120 ns/iter (+/- 16,928)
+// test bench_tiny_sequential  ... bench:          56 ns/iter (+/- 2)
+
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
     let slice_size = input.len() / worker_count;
     let remainder = input.len() % worker_count;
