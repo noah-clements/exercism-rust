87e59ea46e63a163d4e5be952fdc7a90a6fc447b
noah-clements <noahclements@gmail.com>
2023-04-10 14:29:48 -0400

    revised to use u64::MAX

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -6,5 +6,9 @@ pub fn square(s: u32) -> u64 {
 }
 
 pub fn total() -> u64 {
-    (((1u128) << 64) - 1) as u64
+    // The equation is 2^(n+1) - 1 counting from zero
+    // 2^(n+1) is the same as 1 << (n+1)
+    // 1 << 64 would overflow by one, one less is max value of u64
+    // (((1u128) << 64) - 1) as u64
+    u64::MAX
 }
